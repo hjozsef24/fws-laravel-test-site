f5c8cac40830f16b5f5736247dcab64708e101ff
hajdu.jozsef <hjozsef24@gmail.com>
2026-09-10 20:56:47 +0200

    Add contact messages admin panel

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
-    return view('home');
+	return view('home');
 });
 
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
