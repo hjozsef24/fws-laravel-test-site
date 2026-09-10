12243f03fe18f5c0d89e36bb670dcc0f3a9fa0e2
hajdu.jozsef <hjozsef24@gmail.com>
2026-09-10 14:27:16 +0200

    Add header and footer components
    Also added the fonts and the base component files

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,61 @@
+<footer class="w-full bg-[#16161A] pt-20">
+	<div class="mx-auto flex max-w-screen-2xl">
+		<div class="w-[624px]">
+			<a href="{{ url('/') }}">
+				<img src="{{ asset('images/logo-white.svg') }}" alt="Logo" class="w-12 h-auto">
+			</a>
+
+			<p class="mt-4 font-sans text-sm font-normal leading-6 text-[#8D8D96]">
+				Ipari formatervező stúdió Budapesten.
+			</p>
+		</div>
+
+		<div class="w-[375px] ml-12">
+			<h3 class="font-mono text-xs font-medium leading-5 tracking-wider text-[#75757E]">
+				Menü
+			</h3>
+
+			<nav class="mt-5 flex flex-col gap-4">
+				<a href="#references" class="font-sans text-sm font-normal leading-6 text-[#C2C2C8]">
+					Munkáink
+				</a>
+
+				<a href="#" class="font-sans text-sm font-normal leading-6 text-[#C2C2C8]">
+					Studio
+				</a>
+
+				<a href="#" class="font-sans text-sm font-normal leading-6 text-[#C2C2C8]">
+					Folyamat
+				</a>
+
+				<a href="#" class="font-sans text-sm font-normal leading-6 text-[#C2C2C8]">
+					Kapcsolat
+				</a>
+			</nav>
+		</div>
+
+		<div class="ml-12">
+			<h3 class="font-mono text-xs font-medium leading-5 tracking-wider text-[#75757E]">
+				Kapcsolat
+			</h3>
+
+			<div class="mt-5 flex flex-col font-sans text-sm font-normal leading-6 text-[#C2C2C8]">
+				<p>1061 Budapest Fém utca 99.</p>
+				<a href="mailto:studio@fem.hu">studio@fem.hu</a>
+				<a href="tel:+3612345678">+36 1 234 5678</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="mx-auto mt-14 max-w-screen-2xl border-t border-[#FFFFFF1F] pt-6 pb-8">
+		<div
+			class="flex items-center justify-between font-mono text-xs font-normal leading-5 tracking-wide text-[#75757E]">
+			<p>© 2026 Minden jog fenntartva.</p>
+
+			<div class="flex gap-4">
+				<a href="#">Adatvédelem</a>
+				<a href="#">Impresszum</a>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,26 @@
+<header class="w-full py-4">
+	<div class="mx-auto flex max-w-screen-2xl items-center justify-between">
+		<a href="{{ url('/') }}">
+			<img src="{{ asset('images/logo-black.svg') }}" alt="Logo" class="w-12 h-auto">
+		</a>
+
+		<div class="flex gap-8">
+			<a href="#references" class="font-mono text-xs font-normal leading-5 tracking-wider">
+				Munkáink
+			</a>
+
+			<a href="#" class="font-mono text-xs font-normal leading-5 tracking-wider">
+				Studio
+			</a>
+
+			<a href="#" class="font-mono text-xs font-normal leading-5 tracking-wider">
+				Folyamat
+			</a>
+		</div>
+
+		<button
+			class="js-open-contact-modal cursor-pointer border border-[#16161A] bg-[#16161A] text-white px-5 py-3 font-mono text-xs font-normal leading-5 tracking-wide">
+			Kapcsolat
+		</button>
+	</div>
+</header>
