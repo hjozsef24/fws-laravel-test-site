a16b63d53314ba47c885d75c8aeb744279f871a6
hajdu.jozsef <hjozsef24@gmail.com>
2026-09-10 21:16:46 +0200

    Add contact messages export

--- app/Filament/Resources/ContactMessages/Tables/ContactMessagesTable.php
+++ app/Filament/Resources/ContactMessages/Tables/ContactMessagesTable.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Resources\ContactMessages\Tables;
 
+use App\Filament\Exports\ContactMessageExporter;
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\ExportAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -35,6 +37,10 @@ class ContactMessagesTable
             ])
             ->toolbarActions([
                 BulkActionGroup::make([]),
+            ])
+            ->headerActions([
+                ExportAction::make()
+                    ->exporter(ContactMessageExporter::class),
             ]);
     }
 }
